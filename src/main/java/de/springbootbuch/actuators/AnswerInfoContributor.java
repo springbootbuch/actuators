8afaa5221125498c621f9d15c3055affabeebdc0
Michael J. Simons <michael@simons.ac>
2017-02-05 17:47:19 +0100

    Add custom info contributor

---
+++ src/main/java/de/springbootbuch/actuators/AnswerInfoContributor.java
@@ -0,0 +1,14 @@
+package de.springbootbuch.actuators;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnswerInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("theAnswer", 6*7);
+    }
+}
