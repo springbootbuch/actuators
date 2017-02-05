ccb25131d30c58c2ec2e40d942177d618ab30073
Michael J. Simons <michael@simons.ac>
2017-02-05 18:48:29 +0100

    Add custom endpoint

---
+++ src/main/java/de/springbootbuch/actuators/CustomEndpoint.java
@@ -0,0 +1,17 @@
+package de.springbootbuch.actuators;
+
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomEndpoint extends AbstractEndpoint<String> {
+
+    public CustomEndpoint() {
+        super("custom", false, true);
+    }
+
+    @Override
+    public String invoke() {
+        return "The answer to everything can be found under /info\n";
+    }
+}
