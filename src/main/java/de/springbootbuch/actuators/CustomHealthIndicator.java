d7afb09d6961cd53ce51daa1b80f3e75d5e6c026
Michael J. Simons <michael@simons.ac>
2017-02-06 14:17:59 +0100

    Add custom health indicator

---
+++ src/main/java/de/springbootbuch/actuators/CustomHealthIndicator.java
@@ -0,0 +1,20 @@
+package de.springbootbuch.actuators;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Arrays;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        final LocalDate now = LocalDate.now();
+        if(Arrays.asList(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(now.getDayOfWeek())) {
+            return Health.outOfService().withDetail("outOfServiceOn", now.getDayOfWeek()).build();
+        }
+        return Health.up().build();
+    }
+}
