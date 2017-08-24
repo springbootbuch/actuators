9df8fd2aff14f2859dcb888fc113955f907e304c
Michael J. Simons <michael@simons.ac>
2017-08-24 22:25:50 +0200

    Add a profile and fix path

--- src/main/java/de/springbootbuch/actuators/SecurityConfig.java
+++ src/main/java/de/springbootbuch/actuators/SecurityConfig.java
@@ -2,10 +2,12 @@ package de.springbootbuch.actuators;
 
 import org.springframework.boot.actuate.autoconfigure.web.ManagementServerProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Profile("custom-actuator-security")
 @Configuration
 @Order(ManagementServerProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig 
@@ -18,9 +20,9 @@ public class SecurityConfig
 			.httpBasic()
 			.and()
 			.authorizeRequests()
-			.antMatchers("/metrics/counter**")
+			.antMatchers("/application/metrics/counter**")
 				.permitAll()
-			.antMatchers("/metrics/**")
+			.antMatchers("/application/metrics/**")
 				.authenticated();
 	}
 }
