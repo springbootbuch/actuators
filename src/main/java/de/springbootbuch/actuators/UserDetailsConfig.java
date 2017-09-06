b7a579223d041a64b9c367974180fad20f971fa7
Michael J. Simons <michael@simons.ac>
2017-09-06 17:34:10 +0200

    Replace old security properties with a custom user details service

---
+++ src/main/java/de/springbootbuch/actuators/UserDetailsConfig.java
@@ -0,0 +1,29 @@
+package de.springbootbuch.actuators;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * Part of springbootbuch.de.
+ * @author Michael J. Simons
+ * @author @rotnroll666
+ */
+@Configuration
+public class UserDetailsConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
+		final InMemoryUserDetailsManager manager
+				= new InMemoryUserDetailsManager();
+		manager.createUser(
+				User.withUsername("test")
+						.password("test")
+						.roles("USER", "ACTUATOR")
+						.build()
+		);
+		return manager;
+	}
+}
